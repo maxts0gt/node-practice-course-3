c2dd811e77253f84ec48471a97464e3b00f8e655
maxts0gt <bazarsadtsogtjargal@gmail.com>
2021-10-12 01:09:17 +0900

    practiced node basics, modules, import, export

---
+++ notes-app/app.js
@@ -0,0 +1,19 @@
+const chalk = require('chalk');
+console.log(chalk.white.bold.underline.bgBlue('Success!'));
+
+// *****************************************************************************//
+
+// const addFunction = require('./utils.js');
+// result = addFunction(2, 3);
+// console.log(result);
+
+// *****************************************************************************//
+// const noteFunction = require('./notes.js');
+// returnedNote = noteFunction();
+// console.log(returnedNote);
+
+// *****************************************************************************//
+
+// const validator = require('validator');
+// console.log(validator.isEmail('max@max.com'));
+// console.log(validator.isURL('haha.com'));
